be5330daa6dfe846bef93318b954ecf29ea978e6
mxsky01 <878406751@qq.com>
2023-06-29 09:51:21 +0700

    修改：     include/linux/sched/user.h 	修改：     kernel/sched/pkg_core.c

--- include/linux/sched/user.h
+++ include/linux/sched/user.h
@@ -14,7 +14,7 @@ struct key;
  * Some day this will be a full-fledged user tracking system..
  */
 struct user_struct {
-	atomic_t __count;	/* reference count */
+	refcount_t __count;	/* reference count */
 	atomic_t processes;	/* How many processes does this user have? */
 	atomic_t sigpending;	/* How many pending signals does this user have? */
 #ifdef CONFIG_FANOTIFY
